3193ee516ef4dca897dc4ff6836f432fab2a15f7
HHSsub <86719426+HHSsub@users.noreply.github.com>
2025-09-07 20:42:29 +0900

    Update youtube-captions-scraper.d.ts

--- src/types/youtube-captions-scraper.d.ts
+++ src/types/youtube-captions-scraper.d.ts
@@ -1,1 +1,8 @@
-declare module 'youtube-captions-scraper';
+declare module 'youtube-captions-scraper' {
+  interface Subtitle {
+    text: string;
+    start: number;
+    dur: number;
+  }
+  export function getSubtitles(options: { videoID: string; lang?: string }): Promise<Subtitle[]>;
+}
